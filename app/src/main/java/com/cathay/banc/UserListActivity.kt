c2248291651d98241c3896ef68912fbfe9724876
jasper <jasper.hk.cheng@gmail.com>
2021-03-30 18:08:38 +0800

    create the prototype(the layout of list, detail and list item...)

---
+++ app/src/main/java/com/cathay/banc/UserListActivity.kt
@@ -0,0 +1,16 @@
+package com.cathay.banc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+    TODO:
+    remove template android icon drawable * 2
+ */
+class UserListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_list)
+    }
+}
