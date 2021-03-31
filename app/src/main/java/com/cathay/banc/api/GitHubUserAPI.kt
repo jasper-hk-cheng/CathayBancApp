c9b6da723a86aae2829a403b370bce569641e2cb
jasper <jasper.hk.cheng@gmail.com>
2021-04-01 00:08:09 +0800

    remove the redundant TODO comments

--- app/src/main/java/com/cathay/banc/api/GitHubUserAPI.kt
+++ app/src/main/java/com/cathay/banc/api/GitHubUserAPI.kt
@@ -6,29 +6,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-/*
-    TODO
-        topic 1 - query string
-        @GET("users/list?sort=desc")
-        or
-        @GET("users/list")
-        fun groupList(@Query("sort") sort:String):Call<List<User>>
-
-    TODO
-        topic 2 - query string as part of path
-        @GET("group/{id}/users")
-        fun groupList(@Path("id") groupId:Int):Call<List<User>>
-
-    TODO
-        topic 3 - packing all query string in a Map object
-        @GET("group/{id}/users")
-        fun groupList(@Path("id") groupId:Int, @QueryMap options:Map<String, String>):Call<List<User>>
-
-    TODO
-        topic 4 - Post request
-        @POST("users/new")
-        fun createUser(@Body user:User):Call<User>
- */
 interface GitHubUserAPI {
     @GET("/users")
     fun getUserList(): Call<List<UserListItem>>
